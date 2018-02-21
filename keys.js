0e3931b18d3877ca34dbcbee400f163e2f451ade
andrewburd <andrewburd2@gmail.com>
2018-02-21 16:54:29 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,20 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'zdMt2nNzL4EFGiN98G1MZa0j4',
+  consumer_secret: 'MKEhSrH9M8DP5Qe7VctFC9zm3jJGXz4Et0Dl7SMdATttvRYWNf',
+  access_token_key: '962717861243473921-wwmiN7XCtSfq1SpF58a4Xkbdt6hE93e',
+  access_token_secret: '6sBIk0jWxyAdZHBZwKsXpFE3QURFSmnfEgukhs3BtBnv7',
+};
+
+ exports.spotify = {
+   id: '3deb25cab1134d68a7da9dc20eebd33c',
+  secret: '0bd86e0d2ab3447394f82b408b49bf53',
+ };
+
+//Consumer Key (API Key)	zdMt2nNzL4EFGiN98G1MZa0j4
+//Consumer Secret (API Secret)	MKEhSrH9M8DP5Qe7VctFC9zm3jJGXz4Et0Dl7SMdATttvRYWNf
+
+//Access Token	962717861243473921-wwmiN7XCtSfq1SpF58a4Xkbdt6hE93e
+//Access Token Secret	6sBIk0jWxyAdZHBZwKsXpFE3QURFSmnfEgukhs3BtBnv7
+
